cf5481bc005263084950dc831c41b047257fa0ff
dinixweb <sd4live@gmail.com>
2022-08-27 14:39:44 +0100

    initial commit

---
+++ src/test/java/io/dinixweb/Springboot/Security/SpringbootSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package io.dinixweb.Springboot.Security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

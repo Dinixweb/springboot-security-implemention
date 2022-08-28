a3e803285cceb0b25331c91243138af72adf3399
dinixweb <sd4live@gmail.com>
2022-08-28 23:11:24 +0100

    feat:updates

---
+++ src/main/java/io/dinixweb/Springboot/Security/security/SecurityConfiguration.java
@@ -0,0 +1,43 @@
+package io.dinixweb.Springboot.Security.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import javax.sql.DataSource;
+
+
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    DataSource dataSource;
+
+
+    protected  void configure(AuthenticationManagerBuilder auth) throws  Exception{
+        // Set your configuration on the auth object
+        auth.jdbcAuthentication()
+                .dataSource(dataSource);
+
+    }
+
+    @Bean
+    public PasswordEncoder getPasswordEncoder(){
+    return NoOpPasswordEncoder.getInstance();
+    }
+
+    protected void configure(HttpSecurity httpSecurity) throws Exception{
+        httpSecurity.authorizeHttpRequests()
+                .antMatchers("/admin").hasRole("ADMIN")
+                .antMatchers("/user").hasAnyRole("USER","ADMIN")
+                .antMatchers("/").permitAll()
+                .and().formLogin();
+    }
+}
